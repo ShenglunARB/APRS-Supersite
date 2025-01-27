162abf99f8afccacae0b07feecf95ea5fa964880
Shenglun Wu <shenglunwu@gmail.com>
2025-01-27 12:02:59 -0800

    changed data level difinition and minor change on saved file

--- code/src/preprocessing_BC.py
+++ code/src/preprocessing_BC.py
@@ -74,7 +74,7 @@ def find_daily_raw_datafile(folder, date):
     dd = date.strftime('%d')
     
     # get all files in the directory when folder and data file exist
-    folderpath = folder + '/Level1A_Raw_Data/' + YY
+    folderpath = folder + '/Level0_Raw_Data/' + YY
 
     # find file in the folder
     filelsit = os.listdir(folderpath)
@@ -95,7 +95,7 @@ def find_daily_raw_datafile(folder, date):
     
 
 
-def daily_raw_data(filepath):
+def daily_raw_data(filepath, date):
     """
     This function is to get daily raw data in one dataframe.
     Input:
@@ -120,6 +120,9 @@ def daily_raw_data(filepath):
 
     df_daily['DATETIME'] = pd.to_datetime(df_daily['DATE'] + ' ' + df_daily['TIME'])
     df_daily = df_daily.set_index('DATETIME')
+
+    # filter data by date
+    df_daily = df_daily[pd.to_datetime(df_daily['DATE']) == pd.to_datetime(date)]
 
     return df_daily
 
@@ -175,7 +178,7 @@ def clean_warning_data(df_daily):
     # clean negative BCX values
     parameter = ['BC1','BC2','BC3','BC4','BC5','BC6', 'BC7']
     for p in parameter:
-        df_daily_nowarning.loc[df_daily_nowarning[p] < 50, p] = np.nan
+        df_daily_nowarning.loc[df_daily_nowarning[p] < -100, p] = np.nan
 
     return df_daily_nowarning
 
@@ -190,7 +193,7 @@ def clean_parameter_column(df_daily_nowarning):
         df_daily_clean: dataframe, daily raw data with useful columns
     """
 
-    parameter_columns = ['DATE', 'DATE',
+    parameter_columns = ['DATE', 'TIME',
                          'BC1','BC2','BC3','BC4','BC5','BC6', 'BC7']
     
     df_daily_clean = df_daily_nowarning.copy()
@@ -203,10 +206,9 @@ def fill_missing_data(df):
     """
     Used to fill in missing time in the resolution '1min'.
     Input:
-        df_avg: dataframe, averaged data
-        average_time: str, time to average (e.g. '1min', '10min')
+        df: dataframe
     Output:
-        df_avg: dataframe, averaged data with missing time filled
+        df: dataframe, data with missing time filled
     """
 
     
@@ -266,7 +268,7 @@ def average_daily_data(df_daily_clean, average_time, std=True, se=True):
     return df_avg
 
 
-def processed_data_folder_level2a(folder):
+def processed_data_folder_level1a(folder):
     """
     Generate folder path to store processed data.
     Input:
@@ -275,14 +277,14 @@ def processed_data_folder_level2a(folder):
         folder: str, folder path to store processed data
     """
 
-    folder_level2a = folder + '/Level2A_Processed_Data_1min'
+    folder_level1a = folder + '/Level1A_Processed_Data_1min'
     if not os.path.exists(folder):
         os.makedirs(folder)
 
-    return folder_level2a
+    return folder_level1a
 
 
-def processed_data_folder_level2b(folder):
+def processed_data_folder_level1b(folder):
     """
     Generate folder path to store processed data.
     Input:
@@ -291,18 +293,18 @@ def processed_data_folder_level2b(folder):
         folder: str, folder path to store processed data
     """
 
-    folder_level2b = folder + '/Level2B_Processed_Data_1hr'
+    folder_level1b = folder + '/Level1B_Processed_Data_1hr'
     if not os.path.exists(folder):
         os.makedirs(folder)
 
-    return folder_level2b
+    return folder_level1b
 
 
-def store_processed_data(folder_level2a, site, species, date, df_avg):
+def store_processed_data(folder_level1a, site, species, date, df_avg):
     """
     Store processed data to the folder.
     Input:
-        folder_level2a: str, folder path to store processed data
+        folder_level1a: str, folder path to store processed data
         site: str, site name (e.g. 'Fresno-Garland Supersite')
         analyzer: str, standard species name
         date: datetime, date to process
@@ -314,10 +316,15 @@ def store_processed_data(folder_level2a, site, species, date, df_avg):
     # create file name
     site_name = site.split('-')[0]
     monitor_name = 'AE33-' + species
+    YY = pd.to_datetime(date).strftime('%Y')
     date = pd.to_datetime(date).strftime('%Y%m%d')
 
     filename = site_name + '_' + monitor_name + '_' + date + '.csv'
-    filepath = folder_level2a + '/' + filename
+    filepath = folder_level1a + '/' + YY + '/' + filename
+
+    # create folder if not exist
+    if not os.path.exists(folder_level1a + '/' + YY):
+        os.makedirs(folder_level1a + '/' + YY)
 
     # rename columns
     df_avg.rename(columns={'DATE':'DATE_UTC', 'TIME':'TIME_UTC'}, inplace=True)
@@ -346,7 +353,7 @@ def main(site, analyzer, date, average_time='1min'):
 
     if filepath is not None:
     
-        df_daily = daily_raw_data(filepath)
+        df_daily = daily_raw_data(filepath, date)
         screen_warning_df_daily(df_daily)
         df_daily_nowarning = clean_warning_data(df_daily)
         
@@ -360,14 +367,14 @@ def main(site, analyzer, date, average_time='1min'):
 
             # process data to 1min resolution
             if average_time == '1min':
-                folder_level2a = processed_data_folder_level2a(folder)
-                store_processed_data(folder_level2a, site, species, date, df_daily_clean)
+                folder_level1a = processed_data_folder_level1a(folder)
+                store_processed_data(folder_level1a, site, species, date, df_daily_clean)
             
             # process data to 1hr resolution
             elif average_time == '60min':
                 df_avg = average_daily_data(df_daily_clean, average_time)
-                folder_level2b = processed_data_folder_level2b(folder)
-                store_processed_data(folder_level2b, site, species, date, df_avg)
+                folder_level1b = processed_data_folder_level1b(folder)
+                store_processed_data(folder_level1b, site, species, date, df_avg)
 
 
 if __name__ == "__main__":
@@ -376,7 +383,7 @@ if __name__ == "__main__":
     #date = '2024-11-01'
     #main(site, analyzer, date, average_time='1min')
 
-    dates = pd.date_range(start='2024-10-12', end='2024-11-21', freq='D')
+    dates = pd.date_range(start='2023-11-16', end='2023-12-31', freq='D')
     
     for date in dates:
         main(site, analyzer, date, average_time='1min')
